7e3fa6983f8a15286aaded7781c8b53bd26bb76a
bhaskardutta95 <36147983+bhaskardutta95@users.noreply.github.com>
2020-05-17 15:19:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const request = require("request-promise");
+const cheerio = require("cheerio");
+
+const app = express();
+app.use(express.json());
+
+
+ async function main(url,exclude){
+    const result = await request.get(url);
+    const $ = cheerio.load(result);
+    let ar = [];
+    
+    $("table > tbody > tr").each((index, element) => {
+        if(index === 0) return true;
+        const td = $(element).find("td");
+        const nodeName = $(td[0]).text().toString().trim();
+        const eventCount = $(td[3]).text();
+
+        if(eventCount > 0 && (nodeName.search("expired") > -1)){
+            ar.push({
+                name : nodeName.replace(exclude,'').toString(),
+                count : eventCount.toString()
+            })
+        }
+    })
+    return JSON.stringify(ar);
+}
+
+const getUrl = (req,res) => {
+    const url = ("https://" + req.params.ip + "/" + req.params.page + "." + req.params.extension).toString();
+    const exclude = (req.params.exclude).toString();
+    async function callForValue(){
+        res.send(`final : ${await main(url,exclude)}`);
+    }
+    callForValue();
+}
+app.get("/:ip/:page/:extension/:exclude", getUrl);
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
